871fa05013f65d39377b866a7f5c73aadfde3d76
Alan Rocha <alanrochaarg2001@gmail.com>
2023-12-15 12:04:16 -0300

    Criando-Animação

--- js/flappy.js
+++ js/flappy.js
@@ -17,10 +17,6 @@ function Barreira(reversa = false){
 
 }
 
-//const b = new Barreira(true)
-//b.setAltura(200)
-//document.querySelector('[tp-flappy]').appendChild(b.elemento)
-
 function ParDeBarreiras(altura, abertura, x){
     this.elemento = novoElemento('div', 'par-de-barreiras')
 
@@ -50,6 +46,42 @@ function ParDeBarreiras(altura, abertura, x){
     this.setX(x)
 }
 
-const b = new ParDeBarreiras(700, 200, 800)
-document.querySelector('[tp-flappy]').appendChild(b.elemento)
+//const b = new Barreira(true)
+//b.setAltura(200)
+//document.querySelector('[tp-flappy]').appendChild(b.elemento)
+function Barreiras(altura, largura, abertura, espaco, notificarPonto){
+    this.pares = [
+        new ParDeBarreiras(altura, abertura, largura),
+        new ParDeBarreiras(altura, abertura, largura + espaco),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 3)
+    ]
 
+    const deslocamento = 3
+
+    this.animar = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX() - deslocamento)
+
+            // quando o elemento sair da área do jogo
+            if(par.getX() < -par.getLargura()){
+                par.setX(par.getX() + espaco * this.pares.lenght)
+                par.sortearAbertura()
+            }
+
+            const meio = largura / 2
+            const cruzouOMeio = par.getX() + deslocamento >= meio 
+                && par.getX() < meio
+            if(cruzouOMeio) notificarPonto()
+        })
+    }
+}
+
+const barreiras = new Barreiras(700, 1200, 200, 400)
+
+const areaDoJogo = document.querySelector('[tp-flappy]')
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+
+setInterval( () => {
+    barreiras.animar()
+}, 20 )
