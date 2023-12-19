a8fd261665f4515197bf976986e7ccbce2457892
Alan Rocha <alanrochaarg2001@gmail.com>
2023-12-19 10:04:04 -0300

    Adicionando-Colisão

--- js/flappy.js
+++ js/flappy.js
@@ -128,6 +128,30 @@ setInterval( () => {
     passaro.animar()
 }, 20 ) */
 
+function estaoSobrePostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top
+
+    return horizontal && vertical
+}
+
+function colidiu(passaro, barreiras){
+    let colidiu = false    
+
+    barreiras.pares.forEach( parDeBarreiras => {
+        const superior = parDeBarreiras.superior.elemento
+        const inferior = parDeBarreiras.inferior.elemento
+
+        colidiu = estaoSobrePostos(passaro.elemento, superior) 
+        ||   estaoSobrePostos(passaro.elemento, inferior)
+    })
+
+    return colidiu
+}
+
 function FlappyBird() {
     let pontos = 0
 
@@ -151,6 +175,10 @@ function FlappyBird() {
         const temporizador = setInterval( () =>{
             barreiras.animar()
             passaro.animar()
+
+            if(colidiu(passaro, barreiras)) {
+                clearInterval(temporizador)
+            }
         }, 20 ) 
     } 
 }
